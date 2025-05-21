1f7aa34259cca108a3ca94799386c822ab8e38fd
oscar <oscar.laguna@stcfenix.com>
2025-05-20 21:28:59 -0500

    Nuevo inicio

--- order-service/dist/queue/redis.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redisConnection = void 0;
-exports.redisConnection = {
-    host: process.env.REDIS_HOST || 'redis',
-    port: parseInt(process.env.REDIS_PORT || '6379', 10),
-};
-console.log('🔌 Redis connection config:', exports.redisConnection);
-//# sourceMappingURL=redis.config.js.map
